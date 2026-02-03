b2e2d4ecd459c57d587d4168c35bd21631aab397
allankouidri <allan.kouidri@gmail.com>
2026-02-03 14:41:27 +0100

    fix: loading error

--- infer_yolo_26_pose.py
+++ infer_yolo_26_pose.py
@@ -3,8 +3,6 @@ Main Ikomia plugin module.
 Ikomia Studio and Ikomia API use it to load algorithms dynamically.
 """
 from ikomia import dataprocess
-from infer_yolo_26_pose.infer_yolo_26_pose_process import InferYolo26PoseFactory
-from infer_yolo_26_pose.infer_yolo_26_pose_process import InferYolo26PoseParamFactory
 
 
 class IkomiaPlugin(dataprocess.CPluginProcessInterface):
@@ -12,18 +10,16 @@ class IkomiaPlugin(dataprocess.CPluginProcessInterface):
     Interface class to integrate the process with Ikomia application.
     Inherits PyDataProcess.CPluginProcessInterface from Ikomia API.
     """
+
     def __init__(self):
         dataprocess.CPluginProcessInterface.__init__(self)
 
     def get_process_factory(self):
         """Instantiate process object."""
+        from infer_yolo_26_pose.infer_yolo_26_pose_process import InferYolo26PoseFactory
         return InferYolo26PoseFactory()
 
     def get_widget_factory(self):
         """Instantiate associated widget object."""
         from infer_yolo_26_pose.infer_yolo_26_pose_widget import InferYolo26PoseWidgetFactory
         return InferYolo26PoseWidgetFactory()
-
-    def get_param_factory(self):
-        """Instantiate algorithm parameters object."""
-        return InferYolo26PoseParamFactory()
